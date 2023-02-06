ad3de6655a28f2314b42f76a939d51b174540b9f
suranjanmaity <suranjanmaity4@gmail.com>
2023-02-06 23:18:34 +0530

    added images to project section & put hover effect

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 let section_project = document.querySelector("#project");
 let project = document.querySelector('.projects');
 let project_item = document.querySelectorAll('.project_item');
-// project_item.forEach(item=> {
-//     item.classList.add('entry-animation');
-//     item.style.display = 'none';
-// });
 project.style.display = 'none';
-
-
 // Create the observer
 const observer = new IntersectionObserver(entries => {
     entries.forEach((entry) => {
         const project_item = entry.target.querySelectorAll('.project_item');
+        // if observed element is visible
         if (entry.isIntersecting) {
-            
             project.style.display = 'flex';
             project_item.forEach(item=> {
                 setTimeout(() => {
@@ -23,7 +17,6 @@ const observer = new IntersectionObserver(entries => {
             });
             return;
         }
-        
         project.style.display = 'none';
         project_item.forEach(item=> {
             item.classList.remove('entry-animation');
