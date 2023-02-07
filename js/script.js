427bd5ab3cd76761e4191c8e82d73248761f11f4
suranjanmaity <suranjanmaity4@gmail.com>
2023-02-07 13:48:13 +0530

    added event listner to heading blocks

--- js/script.js
+++ js/script.js
@@ -26,3 +26,42 @@ const observer = new IntersectionObserver(entries => {
 });
 // Tell the observer which elements to track
 observer.observe(section_project);
+let project_heading = new Array();
+for (let index = 0; index < project_item.length; index++) {
+    project_heading[index] = project_item[index].children[0];
+};
+for (let index = 0; index < project_item.length; index++) {
+    const element = project_item[index];
+    element.addEventListener("mouseover", () => {
+        element.children[1].style.display = "none";
+        if(index==0){
+            project_heading[0].style.color = "#FFBF00";
+            project_heading[0].style.textShadow = "5px 5px 10px darkred";
+        }
+        if(index==1){
+            project_heading[1].style.color = "white";
+            project_heading[1].style.textShadow = "5px 5px 10px  #FF00FF ";
+        }
+        if(index==2){
+            project_heading[2].style.color = "#FCEDDA";
+            project_heading[2].style.textShadow = "5px 5px 10px #EE4E34";
+        }
+        if(index==3){
+            project_heading[3].style.color = "lightgreen";
+            project_heading[3].style.textShadow = "5px 5px 10px black";
+        }
+    });
+    element.addEventListener("mouseout", () => {
+        element.children[1].style.display = "flex";
+        project_heading[0].style.color = "white";
+        project_heading[1].style.color = "white";
+        project_heading[2].style.color = "white";
+        project_heading[3].style.color = "white";
+        project_heading[0].style.textShadow = "none";
+        project_heading[1].style.textShadow = "none";
+        project_heading[2].style.textShadow = "none";
+        project_heading[3].style.textShadow = "none";
+
+    });
+};
+console.log(project_heading);
